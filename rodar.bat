7296f1bfb948d658cb92096a5aa21e47f0eb647e
AugustodoBRT <augustodobrt8@gmail.com>
2025-11-07 03:10:50 -0300

    fix: corrindo erro no windows winerror32

--- rodar.bat
+++ rodar.bat
@@ -1,14 +1,14 @@
 @echo off
-REM --- 1. CONFIGURE SUA API KEY AQUI ---
-REM Cole sua chave entre as aspas
-set "GOOGLE_API_KEY=SUA_CHAVE_API_VAI_AQUI"
 
-echo "Ativando ambiente virtual..."
-call "%~dp0.venv\Scripts\activate.bat"
+echo Ativando ambiente virtual...
+call .venv\Scripts\activate.bat
 
-echo "Iniciando o script Python..."
-REM CORREÇÃO AQUI: adicione "extrator\"
-python "%~dp0extrator\ocr_extrair.py"
+set "GOOGLE_API_KEY=COLE_SUA_CHAVE_COMPLETA_AQUI"
 
-echo "Processo finalizado."
+echo Iniciando o script Python...
+
+
+python extrator\ocr_extrair.py
+
+echo Processo finalizado.
 pause
